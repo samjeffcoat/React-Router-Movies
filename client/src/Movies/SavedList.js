9a953185e178f84d89815810567d7d295fa5c5c2
samjeffcoat <samjeffcoat@gmail.com>
2019-01-28 16:39:34 -0600

    Adds Home functionality for you beable to click on home button and return home

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Link} from "react-router-dom";
 
 export default class SavedList extends Component {
   constructor(props) {
@@ -12,8 +13,10 @@ export default class SavedList extends Component {
         {this.props.list.map(movie => (
           <span className="saved-movie">{movie.title}</span>
         ))}
-        <div className="home-button">Home</div>
-      </div>
+        <div className = "nav-links">
+        <Link to = "/">Home</Link>
+        </div >
+        </div>
     );
   }
 }
