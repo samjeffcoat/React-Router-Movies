8e8179583cba64249fce3621805cfcdbc48c2f7b
samjeffcoat <samjeffcoat@gmail.com>
2019-01-28 16:09:06 -0600

    Adds MovieCard to MovieList and is displaying correctly

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import MovieCard from "./MovieCard";
 
 
 export default class MovieList extends Component {
@@ -33,23 +34,7 @@ export default class MovieList extends Component {
 }
 
 function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
   return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
-    </div>
+ <MovieCard movie= {movie}/>
   );
 }
